caa1820c7006300eed5a71691dfe8b7beff38464
iamjuni <116604152+iamjuni@users.noreply.github.com>
2025-04-07 11:14:37 -0700

    Merge all commits
    Change /bin/bash to /usr/bin/bash
    
    Add files via upload
    
    Fix spelling error
    
    Update README.md
    
    Rework the script
    
    Forgot to change file name to correct one
    
    Make it better
    
    Change to firefox esr for stability

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # remove-snap
-removes snap for ubuntu
+How to run
+1. wget https://raw.githubusercontent.com/iamjuni/remove-snap/refs/heads/main/remove-snap.sh
+2. chmod +x remove-snap.sh
+3. ./remove-snap.sh
---
+++ other-bash-scripts/install-firefox-esr.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+echo "Adding mozilla ppa"
+
+sudo add-apt-repository ppa:mozillateam/ppa
+
+echo "Configuring apt to block snap version of firefox"
+
+cat << EOF | sudo tee /etc/apt/preferences.d/firefox.pref > /dev/null
+Package: firefox
+Pin: version 1:1snap*
+Pin-Priority: -5
+EOF
+
+echo "Removing snap version of firefox"
+
+sudo snap remove firefox || true
+
+echo "Installing firefox"
+
+sudo apt-get install -y firefox-esr
